a6f197ea58aad202012c04645e6c6b6edfd152e1
benjizhai <ben@zhai.ch>
2023-08-30 20:16:25 +0200

    initial commit
    Signed-off-by: benjizhai <ben@zhai.ch>

--- src/CkIcp.sol
+++ src/CkIcp.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-contracts/access/Ownable.sol";
 import "openzeppelin-contracts/security/ReentrancyGuard.sol";
 import "openzeppelin-contracts/utils/cryptography/ECDSA.sol";

3c6966ed8bb0ff5422571a261e296f66b83fd6c6
benjizhai <ben@zhai.ch>
2023-08-30 20:18:44 +0200

    init test
    Signed-off-by: benjizhai <ben@zhai.ch>

--- test/CkIcp.t.sol
+++ test/CkIcp.t.sol
@@ -5,20 +5,10 @@ import "forge-std/Test.sol";
 import "../src/CkIcp.sol";
 
 contract CkIcpTest is Test {
-    Counter public counter;
+    CkIcp public ckicp;
 
-    function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+    function ckIcpSetup() public {
+        ckicp = new CkIcp();
     }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
-    }
 }

a1863122f8eae9b9b7d36de69ad2fa1bcc8f8faf
Paul H. Liu <paul@dfinity.org>
2023-09-23 04:22:53 -0700

    Clean up and documentation

--- script/airdrop-monitor.sh
+++ script/airdrop-monitor.sh
@@ -13,12 +13,10 @@
 #
 # PEM_FILE      Secret key of the identity to use for the call.
 
-AIRDROP_CLAIMED_LOG="airdrop_claimed.log"
-AIRDROP_CLAIMED_TXS="airdrop_claimed.txs"
-INTERVAL_SECS=60
-
 # Max number of transfers allowed in a single call to Airdrop.
 MAX_RECORDS_IN_A_BATCH=20
+
+# Airdrop canister
 AIRDROP_CANISTER_ID=t7tos-nyaaa-aaaad-aadkq-cai
 
 test -z "$ETH_RPC_URL" && echo "Must set ETH_RPC_URL" && exit 1
@@ -26,67 +24,94 @@ test -z "$ETH_FROM" && echo "Must set ETH_FROM" && exit 1
 test -z "$AIRDROP_FROM" && echo "Must set AIRDROP_FROM" && exit 1
 test -z "$PEM_FILE" && echo "Must set PEM_FILE" && exit 1
 
-# Make sure these file exists
+# This file is an append-only log that stores a JSON object per line
+# to submit airdrop claims to Airdrop ETH contract.
+AIRDROP_CLAIMED_LOG="airdrop_claimed.log"
+
+# This file is an append-only log that stores a transaction id per
+# line created from the submission of airdrop claims. The number
+# of lines should be the same as AIRDROP_CLAIMED_LOG to keep the
+# correspondence.
+#
+# Note that the transaction of the last line may not have been
+# finalized. This script will make sure it has either been finalized
+# or definitely failed, before continuing.
+#
+# If the last transaction has failed (i.e. transaction id is not
+# included), a new attempt will be made to submit the last claim again.
+# This means AIRDROP_CLAIM_LOG may have duplicated lines, which is
+# acceptable.
+AIRDROP_CLAIMED_TXS="airdrop_claimed.txs"
+
+# How many seconds of sleep before repeating the monitoring process.
+INTERVAL_SECS=300
+
+## Make sure these file exists
 touch "$AIRDROP_CLAIMED_LOG"
 touch "$AIRDROP_CLAIMED_TXS"
 
-# Sync the participant list by calling get_airdrop method on the Airdrop canister.
+AIRDROP_JSON_PATTERN=airdop_json.%05d
+
+# Sync the airdrop list by calling get_airdrop method on the Airdrop canister.
 #
-# 1. It would convert the result to json and store in file airdrop_json.XXXX.
+# 1. It will convert the result to json and store in file airdrop_json.XXXXX.
 #
-# 2. The XXXX is basically just an incrementing number, but if the result is
-#    unchanged since the last one, no such file will be created.
+# 2. The XXXXX is basically just an ever-incrementing number, but if the
+#    result is unchanged since the last one, no such file will be created.
 #
-# 3. A diff with the already transfered record is returned as a multiline
-#    string (each line is a JSON array [index, address, amount]). Number of
-#    lines is capped at BATCH_NAX.
+# 3. A diff with the already claimed records (derived from AIRDROP_CLAIMED_LOG)
+#    is returned as a multiline string (each line is a JSON array
+#    [index, address, amount]). Number of lines is capped at BATCH_NAX.
 #
 # 4. If anything goes wrong, nothing is returned.
 function sync_participant_list() {
-    # 1. Get latest airdrop list
+    # Get latest airdrop list
     # TODO: call get_airdrop
     local LIST=$(icx --pem "$PEM_FILE" https://ic0.app update --candid airdrop.did "$AIRDROP_CANISTER_ID" get_airdrop '(0)' | idl2json | jq -c '.Ok|map([."0", ."1", ."2"])')
     echo "DEBUG: LIST=$LIST" >>/dev/stderr
-    # local LIST='[["0","0x2c91e73a358e6f0aff4b9200c8bad0d4739a70dd","100"],["1","0x2c91e73a358e6f0aff4b9200c8bad0d4739a70dd","100"]]'
-    # local LIST='[["0","0x2c91e73a358e6f0aff4b9200c8bad0d4739a70dd","100"],["1","0x2c91e73a358e6f0aff4b9200c8bad0d4739a70dd","100"],["2","0x2c91e73a358e6f0aff4b9200c8bad0d4739a70dd","100"],["3","0x2c91e73a358e6f0aff4b9200c8bad0d4739a70dd","100"]]'
     if [[ -n "$LIST" ]]; then
-        # 2. Find the last airdrop list file, if any
+        # Find the last airdrop list file, if any
         local n=0
         local last_file=/dev/null
         while true; do
-            filename=$(printf airdrop_json.%04d "$n")
-            if [[ -s "$filename" ]]; then
-                last_file="$filename"
+            local next_file=$(printf "$AIRDROP_JSON_PATTERN" "$n")
+            if [[ -s "$next_file" ]]; then
+                last_file="$next_file"
                 n=$((n + 1))
             else
                 break
             fi
         done
-        # 3. Compare LIST with last file if known. If changed, save LIST
-        #    to $filename.
+        # Compare LIST with last file if known.
+        # If different (i.e. with extra appends), save LIST to $next_file.
         local DIFF="$(diff <(jq -c .[] "$last_file") <(jq -c .[] <<<"$LIST"))"
         if [[ -n "$DIFF" ]]; then
-            echo "$LIST" >"$filename"
+            echo "$LIST" >"$next_file"
         else
-            filename="$last_file"
+            next_file="$last_file"
         fi
-        # 4. Compare LIST with claimed records, and return new records.
-        DIFF="$(diff <(jq -c .[] "$AIRDROP_CLAIMED_LOG") <(jq -c .[] "$filename"))"
+        # Compare LIST with claimed records, and return new records.
+        DIFF="$(diff <(uniq "$AIRDROP_CLAIMED_LOG" | jq -c .[]) <(jq -c .[] "$next_file"))"
         if [[ -n "$DIFF" ]]; then
-            # DIFF should be a 'NaN,N' line, and the rest lines starts with >
+            # DIFF should be first a 'NaN,N' line, and the rest lines starts with >
             local lines=$(echo "$DIFF" | wc -l)
             local appends=$(echo "$DIFF" | grep '^> ' | wc -l)
             # echo "DEBUG: $lines $appends" >/dev/stderr
             if [[ "$lines" -eq "$((appends + 1))" && "$(echo "$DIFF" | head -n1)" =~ ^[0-9]*a ]]; then
+                # Success! Take MAX_RECORDS_IN_A_BATCH of them
                 echo "$DIFF" | tail -n$((lines - 1)) | sed -e 's/^> //' | head -n$MAX_RECORDS_IN_A_BATCH
             else
-                echo "ERROR: latest list in ${filename} is not an increment of $AIRDROP_CLAIMED_LOG" >/dev/stderr
+                echo "ERROR: latest list in ${next_file} is not an increment of $AIRDROP_CLAIMED_LOG" >/dev/stderr
             fi
         fi
     fi
 }
 
 while true; do
+    # First, wait for the last transaction to be finalized.
+    # This makes sure we either have succeeded all past transactions,
+    # or we'll re-send the last one (if it is known to have failed)
+    # until it becomes successful.
     TX_JSON=$(tail -n1 $AIRDROP_CLAIMED_LOG)
     TX_HASH=$(tail -n1 $AIRDROP_CLAIMED_TXS)
     if [[ -n "$TX_HASH" && -n "$TX_JSON" ]]; then
@@ -144,8 +169,12 @@ while true; do
             sleep 60
         done
     fi
+
+    # Note that as long as we successfully obtained a transaction id, we consider
+    # these claims to be "dropped". If the transaction later fails, it will be
+    # retried until it becomes successful in Step 0.
     echo -n "4. Calling put_airdrop"
-    INDICES=$(jq -c 'map(.[0])' "$AIRDROP_CLAIMED_LOG" | jq -cn '[inputs]' | jq -c add | sed -e 's/"//g' -e 's/,/;/g' -e 's/^\[/vec {/' -e 's/\]$/}/')
+    INDICES=$(uniq "$AIRDROP_CLAIMED_LOG" | jq -c 'map(.[0])' | jq -cn '[inputs]' | jq -c add | sed -e 's/"//g' -e 's/,/;/g' -e 's/^\[/vec {/' -e 's/\]$/}/')
     # It is ok the following fails, because we'll always try again with all indices!
     icx --pem "$PEM_FILE" https://ic0.app update --candid airdrop.did "$AIRDROP_CANISTER_ID" put_airdrop "($INDICES)"
     echo "   Sleep now. Resume in $INTERVAL_SECS seconds.."

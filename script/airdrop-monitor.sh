fc9341753de9d4921df70c4dbcfc99f6eb3f5f93
Paul H. Liu <paul@dfinity.org>
2023-09-25 07:32:54 -0700

    Sort using jq by number not by string

--- script/airdrop-monitor.sh
+++ script/airdrop-monitor.sh
@@ -98,7 +98,7 @@ function sync_participant_list() {
             next_file="$last_file"
         fi
         # Compare LIST with claimed records, and return new records.
-        DIFF="$(diff <(cat "$AIRDROP_CLAIMED_LOG" | jq -c .[] | sort | uniq) <(jq -c .[] "$next_file" | sort | uniq))"
+        DIFF="$(diff <(cat "$AIRDROP_CLAIMED_LOG" | jq -c .[] | jq -cs 'unique|sort_by(.[0]|tonumber)' | jq -c .[]) <(jq -c .[] "$next_file"))"
         if [[ -n "$DIFF" ]]; then
             # DIFF should be first a 'NaN,N' line, and the rest lines starts with >
             local lines
